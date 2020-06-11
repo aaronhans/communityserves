866c04a7037f1b904a7805fa8d8aa1d592fe71a6
Renee Gallison <renee@insight-rx.com>
2020-06-11 08:54:41 -0700

    update favicon. start setting up request details page

--- docs/index.js
+++ docs/index.js
@@ -1,12 +1,23 @@
+var mainView = document.getElementById('main-view');
+var requestDetailView = document.getElementById('request-detail-view');
 var cleanupRequestsDiv = document.getElementsByClassName('cleanup-requests')[0];
-var locationCardTemplate = document.getElementsByClassName('location-card-template')[0];
-var requestCTA = document.getElementsByClassName('request-cta')[0];
+var requestCardTemplate = document.getElementById('request-card-template');
+var requestDetailsTemplate = document.getElementById('request-detail-template');
+var requestCTA = document.getElementsByClassName('request-help-cta')[0];
 var requestDialog = document.getElementById('request-type');
 var aboutLink =  document.getElementsByClassName('about-link')[0];
 var aboutDialog = document.getElementById('about');
 
 for (var i = 0 ; i < 5 ; i++){
-    cleanupRequestsDiv.appendChild(locationCardTemplate.content.cloneNode(true));
+    var requestCardHTML = requestCardTemplate.content.cloneNode(true);
+    requestCardHTML.querySelector('button').addEventListener("click", function(){
+        mainView.classList.add('hide');
+        requestDetailView.classList.remove('hide');    
+        var requestDetailHTML = requestDetailsTemplate.content.cloneNode(true);
+        requestDetailHTML.querySelector('volunteer-count').text = requestCardHTML.querySelector('volunteer-count').text
+        requestDetailView.appendChild(requestDetailHTML);
+    });
+    cleanupRequestsDiv.appendChild(requestCardHTML);
 }
 
 requestCTA.addEventListener("click", function(ev){
