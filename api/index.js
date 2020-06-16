cf2dda93999bb98001cea5eb4894770fa65dd69d
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-06-15 21:58:14 -0700

    add a field

--- api/index.js
+++ api/index.js
@@ -162,6 +162,9 @@ module.exports = async function addToGithub(url, type) {
           }
         }
 
+        if(item["Business name (as listed on Google Maps)"]) {
+          feature.properties.businessName = item["Business name (as listed on Google Maps)"];
+        }
         if(item['latlon']) {
           feature.geometry.coordinates = [item.latlon]
           newJSON.features.push(feature);
