e39cf0508846cc4a1823eb21a0cfbc659446c75f
Aaron Hans <aaron.hans@alpha.ca.gov>
2020-06-15 18:33:02 -0700

    fix api

--- api/index.js
+++ api/index.js
@@ -18,6 +18,104 @@ const githubApiContents = "contents/";
 const githubApiMerges = "merges";
 const githubApiBranches = "branches/";
 
+const Base64 = {	
+  _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",	
+  encode: function (e) {	
+    var t = "";	
+    var n, r, i, s, o, u, a;	
+    var f = 0;	
+    e = Base64._utf8_encode(e);	
+    while (f < e.length) {	
+      n = e.charCodeAt(f++);	
+      r = e.charCodeAt(f++);	
+      i = e.charCodeAt(f++);	
+      s = n >> 2;	
+      o = ((n & 3) << 4) | (r >> 4);	
+      u = ((r & 15) << 2) | (i >> 6);	
+      a = i & 63;	
+      if (isNaN(r)) {	
+        u = a = 64;	
+      } else if (isNaN(i)) {	
+        a = 64;	
+      }	
+      t =	
+        t +	
+        this._keyStr.charAt(s) +	
+        this._keyStr.charAt(o) +	
+        this._keyStr.charAt(u) +	
+        this._keyStr.charAt(a);	
+    }	
+    return t;	
+  },	
+  decode: function (e) {	
+    var t = "";	
+    var n, r, i;	
+    var s, o, u, a;	
+    var f = 0;	
+    e = e.replace(/++[++^A-Za-z0-9+/=]/g, "");	
+    while (f < e.length) {	
+      s = this._keyStr.indexOf(e.charAt(f++));	
+      o = this._keyStr.indexOf(e.charAt(f++));	
+      u = this._keyStr.indexOf(e.charAt(f++));	
+      a = this._keyStr.indexOf(e.charAt(f++));	
+      n = (s << 2) | (o >> 4);	
+      r = ((o & 15) << 4) | (u >> 2);	
+      i = ((u & 3) << 6) | a;	
+      t = t + String.fromCharCode(n);	
+      if (u != 64) {	
+        t = t + String.fromCharCode(r);	
+      }	
+      if (a != 64) {	
+        t = t + String.fromCharCode(i);	
+      }	
+    }	
+    t = Base64._utf8_decode(t);	
+    return t;	
+  },	
+  _utf8_encode: function (e) {	
+    e = e.replace(/\r\n/g, "n");	
+    var t = "";	
+    for (var n = 0; n < e.length; n++) {	
+      var r = e.charCodeAt(n);	
+      if (r < 128) {	
+        t += String.fromCharCode(r);	
+      } else if (r > 127 && r < 2048) {	
+        t += String.fromCharCode((r >> 6) | 192);	
+        t += String.fromCharCode((r & 63) | 128);	
+      } else {	
+        t += String.fromCharCode((r >> 12) | 224);	
+        t += String.fromCharCode(((r >> 6) & 63) | 128);	
+        t += String.fromCharCode((r & 63) | 128);	
+      }	
+    }	
+    return t;	
+  },	
+  _utf8_decode: function (e) {	
+    var t = "";	
+    var n = 0;	
+    var r = (c1 = c2 = 0);	
+    while (n < e.length) {	
+      r = e.charCodeAt(n);	
+      if (r < 128) {	
+        t += String.fromCharCode(r);	
+        n++;	
+      } else if (r > 191 && r < 224) {	
+        c2 = e.charCodeAt(n + 1);	
+        t += String.fromCharCode(((r & 31) << 6) | (c2 & 63));	
+        n += 2;	
+      } else {	
+        c2 = e.charCodeAt(n + 1);	
+        c3 = e.charCodeAt(n + 2);	
+        t += String.fromCharCode(	
+          ((r & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63)	
+        );	
+        n += 3;	
+      }	
+    }	
+    return t;	
+  },	
+};
+
 const committer = {
   name: "artService",
   email: "aaronhans@gmail.com",
@@ -29,8 +127,10 @@ const authHeader = {
 const defaultoptions = () => ({ method: "GET", headers: authHeader });
 
 module.exports = async function addToGithub() {
-
   // art request: https://docs.google.com/spreadsheets/d/e/2PACX-1vTRun2moHUzxeAr4GjxtT9ONvGdjUyFqgR9NJHul3PXAJnteumrigM0p1rMJR7V6prl4mkTUaXiqhHm/pub?gid=1508614744&single=true&output=csv
+  // clean up: https://docs.google.com/spreadsheets/d/e/2PACX-1vTRun2moHUzxeAr4GjxtT9ONvGdjUyFqgR9NJHul3PXAJnteumrigM0p1rMJR7V6prl4mkTUaXiqhHm/pub?gid=563594306&single=true&output=csv
+  // complete cleanup: https://docs.google.com/spreadsheets/d/e/2PACX-1vTRun2moHUzxeAr4GjxtT9ONvGdjUyFqgR9NJHul3PXAJnteumrigM0p1rMJR7V6prl4mkTUaXiqhHm/pub?gid=390860791&single=true&output=csv
+  // art installs: https://docs.google.com/spreadsheets/d/e/2PACX-1vTRun2moHUzxeAr4GjxtT9ONvGdjUyFqgR9NJHul3PXAJnteumrigM0p1rMJR7V6prl4mkTUaXiqhHm/pub?gid=698941003&single=true&output=csv
   let newFile;
   await fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vTRun2moHUzxeAr4GjxtT9ONvGdjUyFqgR9NJHul3PXAJnteumrigM0p1rMJR7V6prl4mkTUaXiqhHm/pub?gid=1508614744&single=true&output=csv')
     .then(response => response.text())
@@ -72,7 +172,7 @@ module.exports = async function addToGithub() {
 
   let githubBranch = "master";
   let githubApiUrl = "https://api.github.com/repos/aaronhans/communityserves/";
-  let fileLocation = 'public/data/art-requests-test.json';
+  let fileLocation = 'public/data/art-requests.json';
 
   const newURL = `${githubApiUrl}${githubApiContents}${fileLocation}?ref=${githubBranch}`;
   console.log(newURL)
@@ -82,11 +182,12 @@ module.exports = async function addToGithub() {
   // if (existingFileResponse.ok) {
     //update
     const json = await existingFileResponse.json();
+    const base64 = Base64.encode(newFile);
 
     let body = {
       committer,
       branch: githubBranch,
-      content: newFile.toString('base64'),
+      content: base64,
       sha: json.sha,
     };
 
